2975db5996be83796f8087a2c5dc0272cd9be4c3
Amin Darestani <darestani.amin@gmail.com>
2024-03-04 18:51:12 +0330

    directiveExercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,BasicHighlightDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'directive-exercise';
+  numbers = [1, 2, 3, 4, 5];
+  oddNumbers = [1,3,5];
+  evenNumbers = [2,4];
+  onlyOdd = false;
 }
